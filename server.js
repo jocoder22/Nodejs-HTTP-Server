bfe635e0de80d87c6e3484f57bf71a6c06c3698c
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-02-25 20:31:10 -0500

    Fix: fix code bug  Changes to be committed: 	modified:   server.js

--- server.js
+++ server.js
@@ -24,16 +24,3 @@ server.on('request', (req, res) => {
 });
 
 server.listen(8080, '127.0.0.1');
-const http = require('http');
-var fs = require('fs');
-
-const webpage = fs.readFileSync('index.html');
-
-var server = http.createServer();
-
-server.on('request', (req, res) => {
-  res.write(webpage);
-  res.end();
-});
-
-server.listen(8080, '127.0.0.1');
