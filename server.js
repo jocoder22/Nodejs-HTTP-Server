ba58cce4a04f11a049ec0b5109ac2343424879ed
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-02-25 20:29:20 -0500

    Feat: Add nodejs server  Changes to be committed: 	new file:   server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+var fs = require('fs');
+
+const webpage = fs.readFileSync('index.html');
+
+var server = http.createServer();
+
+server.on('request', (req, res) => {
+  res.write(webpage);
+  res.end();
+});
+
+server.listen(8080, '127.0.0.1');
+const http = require('http');
+var fs = require('fs');
+
+const webpage = fs.readFileSync('index.html');
+
+var server = http.createServer();
+
+server.on('request', (req, res) => {
+  res.write(webpage);
+  res.end();
+});
+
+server.listen(8080, '127.0.0.1');
+const http = require('http');
+var fs = require('fs');
+
+const webpage = fs.readFileSync('index.html');
+
+var server = http.createServer();
+
+server.on('request', (req, res) => {
+  res.write(webpage);
+  res.end();
+});
+
+server.listen(8080, '127.0.0.1');
