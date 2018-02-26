dc313c4de252867ff4ddaa1db53cbd11fb034f58
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-02-25 20:41:12 -0500

    Feat: Add server console message
     Changes to be committed:
    	modified:   server.js

--- server.js
+++ server.js
@@ -6,19 +6,7 @@ const webpage = fs.readFileSync('index.html');
 var server = http.createServer();
 
 server.on('request', (req, res) => {
-  res.write(webpage);
-  res.end();
-});
-
-server.listen(8080, '127.0.0.1');
-const http = require('http');
-var fs = require('fs');
-
-const webpage = fs.readFileSync('index.html');
-
-var server = http.createServer();
-
-server.on('request', (req, res) => {
+  console.log(`The server is working well!`);
   res.write(webpage);
   res.end();
 });
